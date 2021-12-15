fc583d9f46811f25831c9d75bbebe5a2c9b5ac34
Johan Smet <johan.smet@justcode.be>
2021-12-15 22:26:56 +0100

    Use a custom header to include GLM so we can disable some compiler warnings.

---
+++ src/raytrace/glm.h
@@ -0,0 +1,11 @@
+// raytrace/glm.h - Johan Smet - BSD-3-Clause (see LICENSE)
+
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+
+#include <glm/glm.hpp>
+
+#pragma GCC diagnostic pop

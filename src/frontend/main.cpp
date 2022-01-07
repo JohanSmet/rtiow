b9cf74b9ccfd8eb86bb140505e2e76eff4785fc1
Johan Smet <johan.smet@justcode.be>
2022-01-07 16:42:17 +0100

    Fix build on Windows.

--- src/frontend/main.cpp
+++ src/frontend/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <cstdlib>
 #include <cstdio>
 #include <chrono>
--- src/raytrace/glm.h
+++ src/raytrace/glm.h
@@ -2,11 +2,15 @@
 
 #pragma once
 
+#if defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wtype-limits"
 #pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif
 
 #include <glm/glm.hpp>
 #include <glm/gtc/epsilon.hpp>
 
+#if defined(__GNUC__) || defined(__clang__)
 #pragma GCC diagnostic pop
+#endif

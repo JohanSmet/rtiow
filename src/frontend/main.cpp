0ac735c1843cd2df9145a3a112eb66a8cec1a38e
Johan Smet <johan.smet@justcode.be>
2022-01-08 18:29:22 +0100

    Frontend: add command-line parameters

--- src/frontend/main.cpp
+++ src/frontend/main.cpp
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <initializer_list>
 #include <thread>
 
 #include <raytrace/raytrace.h>
@@ -9,10 +10,38 @@
 static constexpr int MAX_FPS = 60;
 static constexpr int MIN_FRAMETIME_MS = 1000 / MAX_FPS;
 
-static rtiow::RayTracerConfig raytracer_config;
+using argh_list_t = std::initializer_list<const char *const>;
+static constexpr argh_list_t ARG_RESOLUTION_X = {"-x", "--resolution-x"};
+static constexpr argh_list_t ARG_RESOLUTION_Y = {"-y", "--resolution-y"};
+static constexpr argh_list_t ARG_SAMPLES_PER_PIXEL = {"-s", "--samples-per-pixel"};
+static constexpr argh_list_t ARG_MAX_RAY_BOUNCES = {"-b", "--max-ray-bounces"};
+static constexpr argh_list_t ARG_RENDER_WORKERS = {"-w", "--render-workers"};
+static constexpr argh_list_t ARG_THREADS_IGNORE = {"--threads-ignore"};
+static constexpr argh_list_t ARG_THREADS_PERCENT = {"--threads-percentage"};
 
-int main() {
+int main(int argc, char *argv[]) {
 
+	// parameter parsing
+	argh::parser cmd_line;
+	cmd_line.add_params(ARG_RESOLUTION_X);
+	cmd_line.add_params(ARG_RESOLUTION_Y);
+	cmd_line.add_params(ARG_SAMPLES_PER_PIXEL);
+	cmd_line.add_params(ARG_MAX_RAY_BOUNCES);
+	cmd_line.add_params(ARG_RENDER_WORKERS);
+	cmd_line.add_params(ARG_THREADS_IGNORE);
+	cmd_line.add_params(ARG_THREADS_PERCENT);
+	cmd_line.parse(argc, argv);
+
+	// fill out configuration structure
+	rtiow::RayTracerConfig raytracer_config;
+
+	cmd_line(ARG_RESOLUTION_X, raytracer_config.m_render_resolution_x) >> raytracer_config.m_render_resolution_x;
+	cmd_line(ARG_RESOLUTION_Y, raytracer_config.m_render_resolution_y) >> raytracer_config.m_render_resolution_y;
+	cmd_line(ARG_SAMPLES_PER_PIXEL, raytracer_config.m_samples_per_pixel) >> raytracer_config.m_samples_per_pixel;
+	cmd_line(ARG_MAX_RAY_BOUNCES, raytracer_config.m_max_ray_bounces) >> raytracer_config.m_max_ray_bounces;
+	cmd_line(ARG_RENDER_WORKERS, raytracer_config.m_num_render_workers) >> raytracer_config.m_num_render_workers;
+	cmd_line(ARG_THREADS_IGNORE, raytracer_config.m_threads_ignore) >> raytracer_config.m_threads_ignore;
+	cmd_line(ARG_THREADS_PERCENT, raytracer_config.m_threads_use_percent) >> raytracer_config.m_threads_use_percent;
 
 	// create scene
 	rtiow::Scene scene;
